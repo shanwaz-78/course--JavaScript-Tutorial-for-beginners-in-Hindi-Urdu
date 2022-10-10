1b5c119c4d292779458328e8e759708f458b352d
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-10-10 22:48:45 +0530

    Work Changes

--- Js/array.js
+++ Js/array.js
@@ -16,7 +16,8 @@
             document.write("<li>" + ary[a] + "</li>");
         }*/
 
-        document.write("<strong>This is multidimensional Array<br></strong> ");
+        document.write("<br>");
+        document.write("<strong>This is multidimensional Array</strong><br>");
 
       var HS = [
             ["Saleh Majeed",21,"Male","B.Tech"],
@@ -33,3 +34,29 @@
         document.write("</tr>");
       }
       document.write("</table>")
+
+
+     /* document.write("<B>Array<br></b>");
+
+     var HS = ["Saleh Majeed",18,"Male","B.Tech<br>"];
+      document.write(HS);
+
+      document.write("<b>I Modify The Array<br></b>");
+      HS[1] = 19;
+      document.write(HS + "<br>");
+    
+      document.write("<b>I Delete The index 1 of previous array</b><br>");
+      delete HS[3];
+      document.write(HS + "<br>");
+
+      document.write("This is sort array<br>");
+
+      var a = ["Shanwaz","Danish","Uzair","Saleh Majeed"];
+      document.write(a + "<br>");
+      
+      a.sort();
+      document.write(a + "<br>");
+
+      document.write("This is reverse array<br>");
+      a.reverse();
+      document.write(a);*/
