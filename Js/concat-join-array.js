32e6ec67bf868ec8a1255782fbd2d0d61ed4bb7c
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-10-16 01:47:28 +0530

    Work Changes

---
+++ Js/concat-join-array.js
@@ -0,0 +1,12 @@
+document.write("<b>Concat</b><br>");
+
+var a = ["Saleh","Jahid","Mohsin"];
+var b = ["Adnana","Umer","Fayaz","Michail"];
+var c = a.concat(b);
+document.write(c + "<br>");
+
+document.write("<b>Join</b><br>");
+
+var a = ["Saleh","Jahid","Mohsin"];
+var b = a.join("-");
+document.write(b);
