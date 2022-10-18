2cd021ce0ccfcb71fa2094486229c116ed66c54c
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-10-18 13:16:54 +0530

    Work Changes & add some links in README file too

--- Js/slice-splice-array.js
+++ Js/slice-splice-array.js
@@ -3,12 +3,11 @@
 var a = ["Saleh","Danish","Fayaz","Hussain","Mujahid"];
 document.write(a + "<br>");
 var b = a.slice(1,4);
-document.write(b);
+document.write(b + "<br>");
 
 //document.write("Splice Array");      
 
 var a = ["Saleh","Danish","Fayaz","Hussain"];
 document.write(a + "<br>");
-
 a.splice(-4,1,"Ibrahim");
 document.write(a);
