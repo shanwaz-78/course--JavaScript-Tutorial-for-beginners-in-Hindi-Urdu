4fc9a2662452b390680a27ef7295624cacec4a5d
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-10-21 15:13:29 +0530

    Work Changes & create 2 new js file & add their links in README file too

--- Js/function.js
+++ Js/function.js
@@ -1,28 +1,39 @@
-       console.log("Let's see Function");
+console.log("Let's see Function");
 
-        function Yahoo(){
-            console.log("This is Function");
-        }
+function Yahoo() {
+   console.log("This is Function");
+}
 
-        Yahoo();
-         
-         function name(fname, lname) {
-            console.log(fname + lname);
-         }
-         
-         name("learners" , "learners");
+Yahoo();
 
-         document.write("Let's see event");
-         function learners(){
-            document.write("This is Event in JavaScript<br>");
-         } 
+function name(fname, lname) {
+   console.log(fname + lname);
+}
 
-         function table(num) {
-            for(let a = 1; a<= 10; a++){
-               document.write(num * a + "<br>");
-            }
-         }
+name("learners", "learners");
 
-         table(3);
+document.write("Let's see event");
+function learners() {
+   document.write("This is Event in JavaScript<br>");
+}
 
-    
+function table(num) {
+   for (let a = 1; a <= 10; a++) {
+      document.write(num * a + "<br>");
+   }
+}
+
+table(3);
+
+function names(data) {
+   for (let hs = 0; hs < data.length; hs++) {
+      console.log(data[hs])
+   }
+}
+
+names(["Saleh", "Danish"]);
+names(["Moien", "Aman"]);
+
+
+
+
