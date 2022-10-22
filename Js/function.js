f39505216bb71ec2bdbf29923d9d8dc7a715d836
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-10-22 09:20:00 +0530

    Work Changes & create another one file

--- Js/function.js
+++ Js/function.js
@@ -1,4 +1,4 @@
-console.log("Let's see Function");
+/*console.log("Let's see Function");
 
 function Yahoo() {
    console.log("This is Function");
@@ -15,24 +15,15 @@ name("learners", "learners");
 document.write("Let's see event");
 function learners() {
    document.write("This is Event in JavaScript<br>");
-}
+}*/
 
 function table(num) {
    for (let a = 1; a <= 10; a++) {
-      document.write(num * a + "<br>");
+      console.log(num * a);
    }
 }
 
 table(3);
-
-function names(data) {
-   for (let hs = 0; hs < data.length; hs++) {
-      console.log(data[hs])
-   }
-}
-
-names(["Saleh", "Danish"]);
-names(["Moien", "Aman"]);
 
 
 
--- Js/test.js
+++ Js/test.js
@@ -1,9 +1,9 @@
-function table(array) {
+/*function table(array) {
     for (let a = 0; a < array.length; a++) {
         for (let b = 1; b <= 10; b++) {
             let c = array[a] * b;
             var str = " ";
-            if (c <= 10) {
+            if (c >= 15) {
                 for(let d = 0; d<c; d++){
                     str += "*";   
                 } 
@@ -15,4 +15,16 @@ function table(array) {
 }
 
 
-table([1, 7, [5, 8], [3, 5, 2, [9, 8, 2, 11, [2]]], [8, 9], 13, 6, 8]);
+table([1,3,5,7,8,11,13,15]);
+
+mirror([0, 2, 4, 6]) ➞ [0, 2, 4, 6, 4, 2, 0]
+
+mirror([1, 2, 3, 4, 5]) ➞ [1, 2, 3, 4, 5, 4, 3, 2, 1]
+
+mirror([3, 5, 6, 7, 8]) ➞ [3, 5, 6, 7, 8, 7, 6, 5, 3]*/
+
+let array = [0, 2, 4, 6];
+array.splice(4, 0, 4, 2, 0);
+
+console.log(array);
+
