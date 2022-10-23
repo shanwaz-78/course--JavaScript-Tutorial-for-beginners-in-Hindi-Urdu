805d40daf86032eda87b881ba539da2acfd7e34c
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-10-23 14:59:52 +0530

    Work Changes & debug some issues

--- Js/Object.js
+++ Js/Object.js
@@ -1,13 +1,13 @@
 let obj = {
-    fname : "Shanwaz",
-    lname : "Khan",
-    Age : 18,
+    fname: "Shanwaz",
+    lname: "Khan",
+    Age: 18,
     Qualification: "BCA",
-    Array : ["Saleh",21,"Male","B.Tech"],
-    salary : function(){
+    Array: ["Saleh", 21, "Male", "B.Tech"],
+    salary: function () {
         return 200000;
     },
-    otp : function () {
+    otp: function () {
         return this.fname + " " + this.lname;
     },
 };
