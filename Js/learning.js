da05fe8087e837f962364cfa1302c89361a118a4
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-10-25 00:29:57 +0530

    Work Changes & add some new files

---
+++ Js/learning.js
@@ -0,0 +1,46 @@
+var name = "Shanwaz";
+let fav = "English";
+const age = 18;
+console.log(name);
+console.log(fav);
+console.log(age);
+var name = "name";
+console.log(name);
+name = "name1";
+console.log(name);
+fav = "sanskrit";
+//connsole.log(fav);
+fav = "hindi";
+console.log(fav);
+//age = age+1;
+console.log(age);
+
+const intro = "hi my name is " + name + " " +"and my favourite subject is " + fav + " My age is " + age;
+console.log(intro);
+function fn(paramiter1,paramiter2) {
+    return paramiter1 + paramiter2 * 2;
+}
+const result = fn(2,4);
+console.log(result);
+
+function emptyFunction() {
+   return 1; 
+}
+
+const result1 = emptyFunction();
+console.log(result1);
+//new work of function
+
+function fn(num,num2,num3) {
+    return num + num2 + num3;
+}
+
+console.log(fn(10,20,30));
+
+function per(scmarks) {
+    return scmarks / 600 * 100;
+}
+
+console.log(per(302));
+
+
