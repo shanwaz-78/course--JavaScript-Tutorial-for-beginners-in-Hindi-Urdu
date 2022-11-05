9870f43ee034b2f960845801cdba1fd694b78742
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-11-05 23:41:52 +0530

    work changes & made DOM file

---
+++ Js/DOMmethods.js
@@ -0,0 +1,23 @@
+/*-document.all;
+document.head;
+document.body;
+ document.links;
+document.images;
+document.URL;
+document.title;
+document.domain;*/
+
+   // targeting methods in js.
+
+// document.getElementById("give id name");
+// document.getElementsByClassName("give class name");
+// document.getElementsByTagName("give tag name");
+
+    // DOM get & set moethods;
+    // get method;
+// document.getElementById("header").innerHTML;
+// document.getElementById("header").innerText;
+// document.getElementById("header").getAttribute("id");
+
+    // set method;
+
