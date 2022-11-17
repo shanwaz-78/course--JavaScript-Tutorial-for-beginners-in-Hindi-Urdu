540da7048f619d0582a117012c05ffb79db495cb
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-11-17 11:45:56 +0530

    work changes & made another one file called Templeteliterels.js

---
+++ Js/Templeteliterels.js
@@ -0,0 +1,24 @@
+const nval = "Shanwaz";
+const nval2 = "Khan";
+
+console.log(`${nval} ${nval2}`);
+
+ // Arrow Function.
+
+    const val = (fname,lname) =>`${fname} ${lname}`
+
+    console.log(val("Saleh","Majeed"));
+
+  const val2 = fname => `${fname}`; console.log(val2("Danish"))
+
+    const val3 = lname => `${lname}`; console.log(val3("chouhan"));
+
+    const checkval = (fname,lname) => `${fname} ${lname}`
+    console.log(checkval("Programming is mah passion","JavaScript"))
+
+    const check = (first,second) => `${first} ${second}`
+
+    console.log(check("Fayaz","Adnan"))
+
+    const hs = (fname,lname) => `${fname} ${lname}`
+    console.log(hs("Younus","Lukman"))
