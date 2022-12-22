df841c607ffb04863120cc57e3a8156297404a19
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-12-22 11:19:48 +0530

    work changes in previous file

--- Js/even-odd-number.js
+++ Js/even-odd-number.js
@@ -1,13 +1,13 @@
-document.write("This is even number<br>");
+// document.write("This is even number<br>");
 for(var a = 1; a <= 10; a++){
   if (a % 2 == 0) {
-    document.write(a + "<br>");
+    console.log(a)
   }
 }
 
-document.write("This is odd number<br>");
+// document.write("This is odd number<br>");
 for(var a = 1; a <= 10; a++){
   if (a % 2 != 0 ) {
-    document.write(a + "<br>");
+    console.log(a)
   }
 }
