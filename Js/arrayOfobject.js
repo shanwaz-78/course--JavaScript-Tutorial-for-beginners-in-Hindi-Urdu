5d123164a58daa61944a84d9e6c8f482d042ff72
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-12-29 16:11:22 +0530

    did some practice of arrOfObj

--- Js/arrayOfobject.js
+++ Js/arrayOfobject.js
@@ -4,8 +4,10 @@ const arrOFobj = [
     { name: "Saleh", Age: 21 },
 ];
 
-
-for(let check = 0; check < arrOFobj.length; check++){
-    console.log(arrOFobj[check].name + " " +  arrOFobj[check].Age)
+for(let a in arrOFobj){
+    console.log(`${arrOFobj[a].name} ${arrOFobj[a].Age}`)
 }
+// for(let check = 0; check < arrOFobj.length; check++){
+//     console.log(arrOFobj[check].name + " " +  arrOFobj[check].Age)
+// }
 
