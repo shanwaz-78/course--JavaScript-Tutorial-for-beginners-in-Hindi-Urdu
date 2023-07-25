0d3b5659f125fe9c049520492b1516c7511157de
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2023-07-25 12:29:24 +0530

    Learned new Stuffs

--- Js/fecthApi.js
+++ Js/fecthApi.js
@@ -3,46 +3,64 @@
 // .then(response => response.text()) // It will return the promise
 // .then(result => console.log(result)) // It will also return the promise
 
-console.log('\n');
+console.log("\n");
 // Get from JSON file;
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(resoponse2 => resoponse2.json())
-.then(result2 => console.log(result2))
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((resoponse2) => resoponse2.json())
+  .then((result2) => console.log(result2));
 
-console.log('\n');
-// read only names data of json file 
+console.log("\n");
+// read only names data of json file
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(response3 => response3.json())
-.then(result3 => {
-    for(const i in result3){
-        console.log(result3[i].name)
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response3) => response3.json())
+  .then((result3) => {
+    for (const i in result3) {
+      console.log(result3[i].name);
     }
-}).catch(error => console.log('Error Is Occured'));
+  })
+  .catch((error) => console.log("Error Is Occured"));
 
-console.log('\n');
+console.log("\n");
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(response4 => response4.json())
-.then(result4 => {
-    for(const i in result4){
-        console.log(`${result4[i].username} ${result4[i].email}`)
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response4) => response4.json())
+  .then((result4) => {
+    for (const i in result4) {
+      console.log(`${result4[i].username} ${result4[i].email}`);
     }
-})
+  });
 
-console.log('\n');
+console.log("\n");
 
-fetch('https://jsonplaceholder.typicode.com/users',{
-    method : 'POST',
-    body : JSON.stringify({
-        name : 'Shnwaz',
-        Age : 19,
-        Designation : 'Developer',
-    }),
-    headers : {
-        'Content-type' : 'application/json',
-    },
+fetch("https://jsonplaceholder.typicode.com/users", {
+  method: "POST",
+  body: JSON.stringify({
+    name: "Shnwaz",
+    Age: 19,
+    Designation: "Developer",
+  }),
+  headers: {
+    "Content-type": "application/json",
+  },
 })
-.then(response5 => response5.json())
-.then(result5 => console.log(result5))
+  .then((response5) => response5.json())
+  .then((result5) => console.log(result5));
+
+// get Data
+async function get_data(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Error in Fetching... status code is " + response.status);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+get_data('https://jsonplaceholder.typicode.com/users');
+
